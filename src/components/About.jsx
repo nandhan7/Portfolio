6bbe3b35ffe8c58bc585ecd4b47d4572609bc0e5
nandhan7 <89990618+nandhan7@users.noreply.github.com>
2025-08-03 13:33:46 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -37,14 +37,8 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
-        I am a dedicated software developer with 1 year of experience
-        specializing in full-stack development using the MERN stack. My
-        expertise spans creating efficient, scalable web applications with a
-        focus on both frontend and backend technologies. I have experience
-        building user-friendly interfaces, developing RESTful APIs, and
-        optimizing database performance. I’m passionate about coding and always
-        looking to improve my skills while solving challenging problems through
-        clean, efficient code.
+        I am a  Data Engineer with 2 years of hands-on experience and 6 months of internship in designing and implementing scalable data pipelines, building ETL workflows, and managing cloud-based data infrastructure. I specialize in working with Python, SQL, and Azure services such as Data Factory, Data Lake, and Functions to automate complex data processes and drive data reliability, performance, and analytics.
+Alongside my data engineering work, I also have experience as a Full-Stack Developer using the MERN stack,Python, where I’ve developed RESTful APIs, dynamic React.js frontends, and optimized backend systems for performance and usability. My ability to work across the data and application layers allows me to bridge the gap between raw data and actionable insights.I'm passionate about solving real-world problems through clean, efficient code — whether it's building data-driven platforms, automating pipelines, or creating intuitive user experiences.
       </motion.p>
       <div className="mt-20 flex flex-wrap gap-10">
         {services.map((service, index) => (
